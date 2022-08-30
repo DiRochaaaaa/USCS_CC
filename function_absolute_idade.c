8c926429fff2724505479b9923129383d8208a17
Diego Rocha <99055019+DiRochaaaaa@users.noreply.github.com>
2022-08-29 22:21:03 -0300

    Add files via upload

---
+++ function_absolute_idade.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+
+int idade(){
+	int i;
+	
+	printf("Digite uma idade: ");
+		scanf("%d", &i);
+	
+	while(i<=0 || i>=110){
+		printf("Digite uma idade coerente: ");
+		scanf("%d", &i);
+	}
+	return i;
+}
+
+int abs(int x){  
+	if (x < 0) x = (-1) * x;
+	return x;
+}
+
+
+int main(){
+	 setlocale(LC_ALL, "Portuguese");
+	int id1, id2, dif;
+	
+	id1 = idade();
+	id2 = idade();
+		dif = id1 - id2;
+		printf("Dif: %d", abs(dif));
+
+	return 0;
+}
