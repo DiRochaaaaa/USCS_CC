cf1b6f6b8270ba60c493cd2652096b5f6bc14738
Diego Rocha <99055019+DiRochaaaaa@users.noreply.github.com>
2022-09-09 20:26:53 -0300

    Add files via upload

---
+++ C#_encapsulacao/pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class pessoa
+    {
+        private String nome;
+        private int idade;
+        private char sexo;
+
+        //---------------------------
+        public void setNome (String nome)
+        {
+            this.nome = nome;
+        }
+
+        public String getNome()
+        {
+            return (this.nome);
+        }
+
+        //----------------------------
+
+        public void setIdade (int idade)
+        {
+            this.idade = idade;
+        }
+
+        public int getIdade()
+        {
+            return (this.idade);
+        }
+        //--------------------------
+
+        public void setSexo (char sexo)
+        {
+            this.sexo = sexo;
+        }
+
+        public char getSexo()
+        {
+            return (this.sexo);
+        }
+    }
+}
