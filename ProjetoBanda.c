6298c360c79833b7208c36e4a23d1934fcb7d34a
Diego Rocha <99055019+DiRochaaaaa@users.noreply.github.com>
2022-10-10 22:49:37 -0300

    Update ProjetoBanda.c

--- ProjetoBanda.c
+++ ProjetoBanda.c
@@ -5,6 +5,7 @@ Diego Rocha Guilien Rodrigues
 *******************************************************************************/
 #include <stdio.h>
 #define TAM 3
+#include <string.h>
 
  typedef struct Banda{
         char nome[50];
@@ -29,7 +30,25 @@ Diego Rocha Guilien Rodrigues
         printf("Numero de integrantes: %d\n", show[i].qnt); 
         printf("Posição no ranking %d \n", show[i].rank); 
     }
-}
+    }
+    
+    void PesquisaBanda (BANDA *busca){
+        char aux[50];
+        int achou=0;
+        printf("\nInforme o estilo de musica");
+        scanf("%s", aux);
+            
+        
+         for(int i=0;i<TAM;i++){
+             if(strcmp(busca[i].tipo,aux)==0){
+                achou=1;
+                printf("\nBanda: %s", busca[i].nome);
+            }
+         }
+         if(achou) printf("\nBanda Encontrada!");
+         else printf("\nBanda AUsente");
+    }
+
 int main()
 {
    
@@ -38,7 +57,7 @@ int main()
     LeBanda(b);
     printf("\n\n---------------Lista de bandas:-------------------\n\n");
     MostraBanda(b);
-    
-    
+    printf("\n\n---------------Pesquise sua banda:-------------------\n\n");
+    PesquisaBanda(b);
     return 0;
 }
